0e24a5fb4cc8b75b406f3f5ba764d235f2f6a397
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-28 13:39:00 -0500

    for rendering components

--- react-recorder/src/App.jsx
+++ react-recorder/src/App.jsx
@@ -1,9 +1,32 @@
-import { useState } from 'react'
-
+import { useState, useRef } from 'react'
+import VideoRecorder from './VideoRecorder'
+import AudioRecorder from './AudioRecorder'
 import './App.css'
 
-function App() {
- 
+const App = () => {
+  let [recordOption, setRecordOption] = useState('video')
+  const toggleRecordOption = (type) => {
+    return () => {
+      setRecordOption(type)
+    }
+  }
+
+  return (
+    <div>
+      <h1> React Media Recorder </h1>
+      <div className='button-flex'>
+        <button onClick={toggleRecordOption('video')}>
+          Record Video
+        </button>
+        <button onClick={toggleRecordOption('audio')}>
+          Record Audio
+        </button>
+      </div>
+      <div>
+        {recordOption === 'video' ? <VideoRecorder /> : <AudioRecorder /> }
+      </div>
+    </div>
+  )
 }
 
 export default App
